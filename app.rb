fe374a2c002cfe32f1ff3ae54f3650aa537d9fbf
Heather Forester <hforeste07@gmail.com>
2025-05-10 17:22:16 +0000

    stopping point

--- app.rb
+++ app.rb
@@ -1,9 +1,28 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+  erb(:square_root_results)
+end
+
+get("/payment/new") do
+  erb(:payment_with_form)
+end
+
+
+
+get("/random/new") do
+  erb(:random_with_form)
 end
